6dc77fb800f8b6c5c3f50d818775353a283ebffd
HilesRenton <reneaaron.hilli@gmail.com>
2020-11-23 15:49:34 +0200

    Chapter 3.1

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,13 +1,34 @@
 package com.example.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo (BookRepository repository){
+		
+		return(args) -> {
+			Book b1 = new Book("Jussi tarina","jussi", "1997", "951-98548-9-4", "9.50" ) ;
+			Book b2 = new Book("Rene tarina","Rene", "2001", "951-98728-10-4", "12.50" ) ;
+			Book b3 = new Book("Lotr","Tolkien", "1967", "951-98548-9-4", "19.99" ) ;
+			
+			
+			repository.save(b1) ;
+			repository.save(b2) ;
+			repository.save(b3) ;
+	};
+		
 	}
 
 }
