96f453a1d0b625c9cb96823955df64c237a77473
HilesRenton <reneaaron.hilli@gmail.com>
2020-11-23 12:48:56 +0200

    First commit

---
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -0,0 +1,9 @@
+package com.example.bookstore.domain;
+
+public class Book {
+	public String title ;
+	public String author ;
+	public String year ;
+	public String isbn ;
+	public String price ;
+}
